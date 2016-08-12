c78931da5d1b77da5ad20430606b6f4d20cc30ea
chaddbennett <chadd.d.bennett@gmail.com>
2016-08-12 15:43:01 -0700

    [aws] fixed duplicate actiontype declaration

--- client/actions/actionTypes.js
+++ client/actions/actionTypes.js
@@ -11,10 +11,6 @@ export const CLEAR_MENTOR = 'clear_mentor';
 /* MENTORS */
 export const MENTOR_FETCH_REVIEWS = 'mentor_fetch_reviews';
 
-/* API/APPOINTMENTS */
-export const CREATE_APPOINTMENT = 'create_appoinment';
-export const FETCH_APPOINTMENTS = 'fetch_appointments';
-
 /* API/REVIEWS */
 export const SUBMIT_REVIEW = 'submit_review';
 export const CURRENT_MENTOR_REVIEWS = 'current_mentor_reviews';
